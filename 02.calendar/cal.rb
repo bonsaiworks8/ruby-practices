6c7dbf9bc2df57ca48893a9b4638e5ef78e0ab8b
bonsaiworks8 <bonsaiworks8@gmail.com>
2021-10-02 18:27:24 +0900

    Add spaces, add freeze to constants, change repeated statements from while to each, change initialization logic, etc.

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,18 +4,18 @@ require 'date'
 require 'optparse'
 
 class MyCalender
-  DAY_OF_WEEK = ['日', '月', '火', '水', '木', '金', '土']
+  DAY_OF_WEEK = ['日', '月', '火', '水', '木', '金', '土'].freeze
 
   # ANSIカラーエスケープシーケンス
-  FG_BLACK = "\e[30m"
-  BG_WHITE = "\e[47m"
-  DEFAULT = "\e[0m"
+  FG_BLACK = "\e[30m".freeze
+  BG_WHITE = "\e[47m".freeze
+  DEFAULT = "\e[0m".freeze
 
-  def initialize(year: Date.today.year, month: Date.today.month)
-    @year = year
-    @month = month
-    @first_date = Date.new(year, month, 1)
-    @end_date = Date.new(year, month, -1)
+  def initialize(year:, month:)
+    @year = (year ||= Date.today.year).to_i
+    @month = (month ||= Date.today.month).to_i
+    @first_date = Date.new(@year, @month, 1)
+    @last_date = Date.new(@year, @month, -1)
   end
 
   def show
@@ -27,33 +27,29 @@ class MyCalender
   private
 
   def format_days
-    days=''
+    days = ''
 
     # 月の最初の日の曜日の位置に至るまで空白を入れる
     @first_date.wday.times do
       days += '　 '
     end
-
-    day = 1
-    while day <= @end_date.day
-      current_date = Date.new(@year, @month, day)
+    
+    (@first_date..@last_date).each do |current_date|
       if current_date == Date.today
         # 今日の日付に色を付ける
         days += (FG_BLACK + BG_WHITE)
       end
 
-      days += day.to_s.rjust(2)
+      days += current_date.day.to_s.rjust(2)
 
-      days+=DEFAULT
+      days += DEFAULT
 
-      if current_date.wday == 6
+      if current_date.saturday?
         # 土曜日なら改行コードを入れる
         days += "\n"
       else
         days += ' '
       end
-
-      day += 1
     end
 
     days
@@ -64,29 +60,21 @@ end
 options = ARGV.getopts('y:m:')
 
 # 入力チェック
-mes=''
+message = ''
 if options["y"]
-  mes = "The year designation is incorrect. Range is between 1970 and 2100.\n" unless options["y"].match(/(^19[7-9][0-9]$)|(^2[0][0-9]{2}$)|(^2100$)/)
+  message = "The year designation is incorrect. Range is between 1970 and 2100.\n" unless options["y"].match(/(^19[7-9][0-9]$)|(^2[0][0-9]{2}$)|(^2100$)/)
 end
 if options["m"]
-  mes += "The month designation is incorrect. Range is between 1 and 12.\n" unless options["m"].match(/(^[1-9]$|^1[0-2]$)/)
+  message += "The month designation is incorrect. Range is between 1 and 12.\n" unless options["m"].match(/(^[1-9]$|^1[0-2]$)/)
 end
 
 # 入力エラーでプログラム終了
-unless mes.empty?
-  puts mes
+unless message.empty?
+  puts message
   exit
 end
 
-if !options["y"] && !options["m"]
-  my_calender= MyCalender.new
-elsif options["y"] && options["m"]
-  my_calender= MyCalender.new(year: options["y"].to_i, month: options["m"].to_i)
-elsif options["y"] && !options["m"]
-  my_calender= MyCalender.new(year: options["y"].to_i)
-else
-  my_calender= MyCalender.new(month: options["m"].to_i)
-end
+my_calender = MyCalender.new(year: options["y"], month: options["m"])
 
 # カレンダー表示
 my_calender.show
