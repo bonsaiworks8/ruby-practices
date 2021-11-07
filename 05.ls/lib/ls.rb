a4f367dfc789a56cfce5b863505b5407d3afa1bf
bonsaiworks8 <bonsaiworks8@gmail.com>
2021-11-08 05:42:13 +0900

    Add -l option

--- 05.ls/lib/ls.rb
+++ 05.ls/lib/ls.rb
@@ -1,15 +1,38 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'optparse'
+require 'etc'
+
 def main
-  Command.new(ARGV[0]).show_list
+  options = ARGV.getopts('l')
+  Command.new(options, ARGV[0]).show_list
 end
 
 class Command
   MAX_COL = 3
   MAX_LENGTH = 24
 
-  def initialize(path)
+  # ファイルの種類(8進数表記と記号)の対応表
+  FILE_TYPE_TABLE = {
+    '04' => 'd',  # ディレクトリ
+    '10' => '-',  # 通常ファイル
+    '12' => 'l'   # シンボリックリンク
+  }.freeze
+
+  PERMISSION_TABLE = {
+    '0' => '---',
+    '1' => '--x',
+    '2' => '-w-',
+    '3' => '-wx',
+    '4' => 'r--',
+    '5' => 'r-x',
+    '6' => 'rw-',
+    '7' => 'rwx'
+  }.freeze
+
+  def initialize(options, path)
+    @options = options
     path ||= '.'
     @path = path
   end
@@ -17,17 +40,21 @@ class Command
   def show_list
     return unless exist?
 
-    files = list_up
+    if @options['l']
+      lines = list_up
+    else
+      files = list_up
 
-    max_row = (files.length / MAX_COL.to_f).ceil
+      max_row = (files.length / MAX_COL.to_f).ceil
 
-    row = 0
-    lines = Array.new(max_row, '')
-    files.each do |file|
-      lines[row] = lines[row] + file.ljust(MAX_LENGTH)
-      row += 1
+      row = 0
+      lines = Array.new(max_row, '')
+      files.each do |file|
+        lines[row] = lines[row] + file.ljust(MAX_LENGTH)
+        row += 1
 
-      row = 0 if row >= max_row
+        row = 0 if row >= max_row
+      end
     end
 
     lines.each do |line|
@@ -46,9 +73,57 @@ class Command
   end
 
   def list_up
-    Dir.children(@path).sort.reject do |file|
-      file.start_with?('.')
+    files = Dir.glob('*', base: @path)
+
+    files = list_up_details files if @options['l']
+
+    files
+  end
+
+  def list_up_details(files)
+    # ハードリンクとファイルサイズ各々で最大の桁数を求める
+    hardlink_digits, size_digits = count_digits files
+
+    total_blocks = 0
+    lines = files.map do |file|
+      full_path = File.join(@path, file)
+      stat = File.stat(full_path)
+
+      # ファイルタイプとファイルモード(6桁8進数表記)を取得
+      mode =  stat.mode.to_s(8).rjust(6, '0')
+      numbers = mode.scan(/(\d{2})(\d)(\d)(\d)(\d)/)
+
+      file_type = FILE_TYPE_TABLE[numbers[0][0]]
+      permission = PERMISSION_TABLE[numbers[0][2]] + PERMISSION_TABLE[numbers[0][3]] + PERMISSION_TABLE[numbers[0][4]]
+      hardlink = stat.nlink.to_s
+      username = Etc.getpwuid(stat.uid).name
+      groupname = Etc.getgrgid(stat.gid).name
+      size = stat.size.to_s
+
+      date_and_time = stat.mtime.month.to_s.concat(' ', stat.mtime.day.to_s, ' ', stat.mtime.hour.to_s, ':', stat.mtime.min.to_s)
+      total_blocks += stat.blocks
+
+      line = +''
+      line.concat(file_type, permission, '  ', hardlink.rjust(hardlink_digits), ' ', username, '  ', groupname, '  ', size.rjust(size_digits), ' ',
+                  date_and_time, ' ', file)
     end
+
+    lines.unshift((+'total ').concat(total_blocks.to_s))
+  end
+
+  def count_digits(files)
+    hardlink_digits = 1
+    size_digits = 1
+
+    files.each do |file|
+      full_path = File.join(@path, file)
+      stat = File.stat(full_path)
+
+      hardlink_digits = stat.nlink.to_s.length if stat.nlink.to_s.length > hardlink_digits
+      size_digits = stat.size.to_s.length if stat.size.to_s.length > size_digits
+    end
+
+    [hardlink_digits, size_digits]
   end
 end
 
--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -5,7 +5,7 @@ require 'minitest/autorun'
 require_relative '../lib/ls'
 
 class LsTest < Minitest::Test
-  # ../sampleディレクトリ内のファイルの出力
+  # ls コマンドでの../sampleディレクトリ内のファイルの出力
   OUTPUT_TEST1 = <<~OUTPUT1.freeze
     Gemfile                 babel.config.js         log#{'                     '}
     Gemfile.lock            bin                     package.json#{'            '}
@@ -13,7 +13,29 @@ class LsTest < Minitest::Test
     README.md               config.ru#{'               '}
   OUTPUT1
 
-  def test_ls
-    assert_output(OUTPUT_TEST1) { Command.new(ARGV[0]).show_list }
+  # ls -l コマンドでの../sampleディレクトリ内のファイルの出力
+  OUTPUT_TEST4 = <<~OUTPUT4
+    total 0
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 Gemfile
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 Gemfile.lock
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 Procfile
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 README.md
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 babel.config.js
+    drwxr-xr-x  2 hideakisuyama  staff  64 10 14 17:55 bin
+    drwxr-xr-x  2 hideakisuyama  staff  64 10 14 17:55 config
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 config.ru
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 log
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 package.json
+    -rw-r--r--  1 hideakisuyama  staff   0 10 14 17:55 postcss.config.js
+  OUTPUT4
+
+  def test_ls1
+    options = {}
+    assert_output(OUTPUT_TEST1) { Command.new(options, ARGV[0]).show_list }
+  end
+
+  def test_ls4
+    options = { 'l' => true }
+    assert_output(OUTPUT_TEST4) { Command.new(options, ARGV[0]).show_list }
   end
 end
