022fdaf4f4b8d0e8fa27af64daaba8efa5b96a32
bonsaiworks8 <bonsaiworks8@gmail.com>
2021-12-07 12:02:45 +0900

    Code has been improved to make it easier to see

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'optparse'
-
 class WcCommand
   MAX_DIGITS = 8
 
@@ -14,59 +12,52 @@ class WcCommand
     if @paths.size.positive?
       total_line_count = 0
       total_word_count = 0
-      total_char_count = 0
+      total_byte_size = 0
       @paths.each do |path|
-        line_count, word_count, char_count = count_each_number path
+        line_count, word_count, byte_size = count_each_number File.readlines(path)
 
         total_line_count += line_count
         total_word_count += word_count
-        total_char_count += char_count
+        total_byte_size += byte_size
 
-        print_each_number path, line_count, word_count, char_count
+        print_each_number line_count, word_count, byte_size, path
       end
 
-      print_each_number 'total', total_line_count, total_word_count, total_char_count if @paths.size > 1
+      print_each_number total_line_count, total_word_count, total_byte_size, 'total' if @paths.size > 1
     else
-      line_count, word_count, char_count = count_each_number nil
+      line_count, word_count, byte_size = count_each_number readlines
 
-      print_each_number nil, line_count, word_count, char_count
+      print_each_number line_count, word_count, byte_size
     end
   end
 
   private
 
-  def print_each_number(path, line_count, word_count, char_count)
-    each_number = if @options['l']
-                    [
-                      line_count.to_s.rjust(MAX_DIGITS),
-                      ' ',
-                      path
-                    ]
-                  else
-                    [
-                      line_count.to_s.rjust(MAX_DIGITS),
-                      word_count.to_s.rjust(MAX_DIGITS),
-                      char_count.to_s.rjust(MAX_DIGITS),
-                      ' ',
-                      path
-                    ]
-                  end
+  def print_each_number(line_count, word_count, byte_size, path = nil)
+    row_data =
+      if @options['l']
+        [format_digits(line_count), ' ', path]
+      else
+        [format_digits(line_count), format_digits(word_count), format_digits(byte_size), ' ', path]
+      end
 
-    puts each_number.join
+    puts row_data.join
   end
 
-  def count_each_number(path)
-    lines = path ? File.readlines(path) : readlines
-
+  def count_each_number(lines)
     line_count = 0
     word_count = 0
-    char_count = 0
+    byte_size = 0
     lines.each do |line|
       line_count += 1
       word_count += line.split(/\s+/).length
-      char_count += line.bytesize
+      byte_size += line.bytesize
     end
 
-    [line_count, word_count, char_count]
+    [line_count, word_count, byte_size]
+  end
+
+  def format_digits(value)
+    value.to_s.rjust(MAX_DIGITS)
   end
 end
